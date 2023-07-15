2949b3f4984d10f55f29e60810b315c8f8972c41
BuildTools <unconfigured@null.spigotmc.org>
2023-07-15 16:42:50 -0400

    Added FabricMain.java

---
+++ fabric-1.17/src/main/java/dev/neuralnexus/xpanimals/fabric/FabricMain.java
@@ -0,0 +1,13 @@
+package dev.neuralnexus.xpanimals.fabric;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.LoggerFactory;
+
+public class FabricMain implements ModInitializer {
+    public static final String MOD_ID = "xpanimals";
+    public static final logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitialize() {
+
+    }
+}
