85ba032824f6e6fef767581defc2e0005d645e89
BuildTools <unconfigured@null.spigotmc.org>
2023-09-22 16:16:05 -0400

    Updated Creative mod tab to Official Logo

--- fabric-1.19/src/main/java/dev/neuralnexus/xpanimals/fabric/item/FabricItemGroup.java
+++ fabric-1.19/src/main/java/dev/neuralnexus/xpanimals/fabric/item/FabricItemGroup.java
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class FabricItemGroup {
-    public static final ItemGroup XPANIMALZ = FabricItemGroupBuilder.build(new Identifier(FabricMain.MOD_ID, "xpanimalz"), () -> new ItemStack(FabricItems.RED_PANDA_SPAWN_EGG));
+    public static final ItemGroup XPANIMALZ = FabricItemGroupBuilder.build(
+            new Identifier(FabricMain.MOD_ID, "xpanimalz"), () -> new ItemStack(FabricItems.XP_LOGO));
 
 }
--- fabric-1.19/src/main/java/dev/neuralnexus/xpanimals/fabric/item/FabricItems.java
+++ fabric-1.19/src/main/java/dev/neuralnexus/xpanimals/fabric/item/FabricItems.java
@@ -10,7 +10,12 @@ import net.minecraft.util.registry.Registry;
 
 public class FabricItems {
 
-    public static final Item RED_PANDA_SPAWN_EGG = registerItem("red_panda_spawn_egg", new SpawnEggItem(FabricEntities.Red_Panda, 0xb6510e, 0x1a1a1b, new FabricItemSettings().group(FabricItemGroup.XPANIMALZ).maxCount(1)));
+    public static final Item RED_PANDA_SPAWN_EGG = registerItem("red_panda_spawn_egg",
+            new SpawnEggItem(FabricEntities.Red_Panda, 0xb6510e, 0x1a1a1b,
+                    new FabricItemSettings().group(FabricItemGroup.XPANIMALZ).maxCount(1)));
+
+    public static final Item XP_LOGO = registerItem("xp_logo",
+            new Item(new FabricItemSettings().group(FabricItemGroup.XPANIMALZ)));
 
     public static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(FabricMain.MOD_ID, name), item);
